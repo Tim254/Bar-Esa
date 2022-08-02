c6ad28907263445a40f782fbbd0500f7a426a8fc
Tim254 <tim.mailu@gmail.com>
2022-08-02 20:54:47 +0300

    creating footer

---
+++ src/UI/Footer.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <hr></hr>
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li>
+                <a href="#">about us</a>
+              </li>
+              <li>
+                <a href="#">our services</a>
+              </li>
+              <li>
+                <a href="#">privacy policy</a>
+              </li>
+              <li>
+                <a href="#">Events Program</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li>
+                <a href="#">shipping</a>
+              </li>
+              <li>
+                <a href="#">returns</a>
+              </li>
+              <li>
+                <a href="#">order status</a>
+              </li>
+              <li>
+                <a href="#">payment options</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>online Bar</h4>
+            <ul>
+              <li>
+                <a href="#">Wholesale</a>
+              </li>
+              <li>
+                <a href="#">Retail</a>
+              </li>
+              <li>
+                <a href="#">Event Supply</a>
+              </li>
+              <li>
+                <a href="#">Baristers</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+              <a href="#">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
