beebf048fe9c0b9e2306d0d90c58a7334e32dcae
Tim254 <tim.mailu@gmail.com>
2022-08-02 20:59:59 +0300

    Adding a modal via a react Portal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Structure/Header";
+import Drinks from "./Drinks/Drinks";
+import Cart from "./Cart-Items/Cart";
+import CartProvider from "./Storage/CartProvider";
+import Footer from "./UI/Footer";
 
 function App() {
+  const [cartIsVisible, setCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartVisible(true);
+  };
+  const hideCartHandler = () => {
+    setCartVisible(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CartProvider>
+        {cartIsVisible && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+          <Drinks />
+        </main>
+        <Footer/>
+      </CartProvider>
+    </Router>
   );
 }
 
