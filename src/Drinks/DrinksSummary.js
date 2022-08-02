c8c90afd263185adaa9b3de28c816d42a6a9d974
Tim254 <tim.mailu@gmail.com>
2022-08-02 20:45:54 +0300

    adding drink summary component

---
+++ src/Drinks/DrinksSummary.js
@@ -0,0 +1,12 @@
+import classes from './DrinksSummary.module.css'
+
+const DrinksSummary = () => {
+    return <section className={classes.summary}>
+        <h2>You order, We Deliver To You in Minutes</h2>
+        <p>Our online bar offers a wide variety of alcoholic drinks and lets you enjoy yoour party
+            at home.
+        </p>
+        <p>All drinks are delivered at your door step, just in time to get the party started</p>
+    </section>
+}
+export default DrinksSummary;
