1b6d74bb31cabc54ee83eb42cf735663ef96bc62
Tim254 <tim.mailu@gmail.com>
2022-08-02 20:46:50 +0300

    moving everything to drinks component

---
+++ src/Drinks/Drinks.js
@@ -0,0 +1,11 @@
+import DrinksSummary from "./DrinksSummary"
+import AvailableDrinks from "./AvailableDrinks"
+import { Fragment } from "react"
+
+const Drinks = () => {
+    return <Fragment>
+        <DrinksSummary/>
+        <AvailableDrinks/>
+    </Fragment>
+}
+export default Drinks;
